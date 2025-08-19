b4e4030dcfb0e90bcaa18b4b50e1fb3bceb845e5
ingjefreypadilla <ing_jefreypadilla@icloud.com>
2025-08-19 08:14:25 -0400

    fixed IcalculateBonus, It should have the correct parameters

--- service_bonus/use_cases/interfaces/calculate_bonus_interface.py
+++ service_bonus/use_cases/interfaces/calculate_bonus_interface.py
@@ -1,8 +1,11 @@
 # service_bonus/use_cases/interfaces/calculate_bonus_interface.py
 from abc import ABC, abstractmethod
 
+from service_bonus.domain.worker import Worker
+
+
 class ICalculateBonus(ABC):
 
     @abstractmethod
-    def calculate(self, name: str, years: int, salary: float) -> float:
+    def calculate(self, worker: Worker, calculated_period, calculated_period_method) -> float:
         pass
